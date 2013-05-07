00797abb6a2d5cb5ec2a3ef6ca5678bc28083ba9
jorawebdev <jorawebdev@gmail.com>
2013-05-06 20:15:50 -0700

    bla

--- js/app.js
+++ js/app.js
@@ -2,10 +2,18 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   // put your routes here
+  this.resource( 'index', { path: '/' } ); // Takes us to &quot;/&quot;
+  this.resource('about', { path: '/about' });
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return ['red', 'yellow', 'blue', 'green'];
+  }
+});
+
+App.AboutRoute = Ember.Route.extend({
+  setupController: function(controller) {
+    controller.set('content', ['web', 'mobile']);
   }
 });
